d1a262c38f8d7fafc5298776d4de5b74aa9c3f02
Brianna <Brianna@BriannaWin10PC.com>
2019-08-13 15:55:25 -0700

    add files
    add basic javascript assignments

---
+++ Basic JavaScript Projects/Basic_JavaScript_1.js
@@ -0,0 +1,14 @@
+var A = "Hello, world!";
+document.write(A);
+ var B = "This is a string"
+ window.alert(B);
+ document.write("\"Don\'t worry,\" Eli said, \"I promise all I\'m going to do is hook my laptop up and say \'hello world\'.\"");
+document.write("<br><i>Da mi basia mille," + " deinde centum," + "<br>dein mille altera," + " dien secunda centum</i>");
+var C = "<br> Legally, 75% of the boards in a boat " + "must change before it is no longer the same boat.<br>"
+document.write(C);
+var Crew = "Enterprise", Capt = "Kirk ", ExecOfficer = "Spock ", CommOfficer = "Uhura "
+var Capt = Capt.fontcolor("yellow"), ExecOfficer = ExecOfficer.fontcolor("blue"), CommOfficer = CommOfficer.fontcolor("red");
+document.write(Capt);
+document.write(ExecOfficer);
+document.write(CommOfficer);
+document.write(3 + 3);
