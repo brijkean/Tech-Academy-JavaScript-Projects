05feef2c3f4781a78e76aa1b5149c99f85a910c7
Brianna <Brianna@BriannaWin10PC.com>
2019-08-14 00:42:39 -0700

    add files

--- Basic JavaScript Projects/Basic_JavaScript_1.js
+++ Basic JavaScript Projects/Basic_JavaScript_1.js
@@ -3,11 +3,11 @@ document.write(A);
  var B = "This is a string"
  window.alert(B);
  document.write("\"Don\'t worry,\" Eli said, \"I promise all I\'m going to do is hook my laptop up and say \'hello world\'.\"");
-document.write("<br><i>Da mi basia mille," + " deinde centum," + "<br>dein mille altera," + " dien secunda centum</i>");
+document.write("<br><i>There was no possibility" + " of taking a walk" + " that day.</i>");
 var C = "<br> Legally, 75% of the boards in a boat " + "must change before it is no longer the same boat.<br>"
 document.write(C);
 var Crew = "Enterprise", Capt = "Kirk ", ExecOfficer = "Spock ", CommOfficer = "Uhura "
-var Capt = Capt.fontcolor("yellow"), ExecOfficer = ExecOfficer.fontcolor("blue"), CommOfficer = CommOfficer.fontcolor("red");
+var Capt = Capt.fontcolor("gold"), ExecOfficer = ExecOfficer.fontcolor("blue"), CommOfficer = CommOfficer.fontcolor("red");
 document.write(Capt);
 document.write(ExecOfficer);
 document.write(CommOfficer);
